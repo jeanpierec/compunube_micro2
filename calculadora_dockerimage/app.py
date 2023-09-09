ca3c0a91eff8069d55f692c79e98512e5cf3e769
Jean Piere Castro <gnpiere@gmail.com>
2023-09-08 19:21:25 -0500

    Entrega microproyecto 2

---
+++ calculadora_dockerimage/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    result = data['num1'] + data['num2']
+    return jsonify({'result': result})
+
+@app.route('/subtract', methods=['POST'])
+def subtract():
+    data = request.get_json()
+    result = data['num1'] - data['num2']
+    return jsonify({'result': result})
+
+@app.route('/multiply', methods=['POST'])
+def multiply():
+    data = request.get_json()
+    result = data['num1'] * data['num2']
+    return jsonify({'result': result})
+
+@app.route('/divide', methods=['POST'])
+def divide():
+    data = request.get_json()
+    if data['num2'] == 0:
+        return jsonify({'error': 'Division by zero is not allowed'})
+    result = data['num1'] / data['num2']
+    return jsonify({'result': result})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
